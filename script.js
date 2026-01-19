e4f5ce669776f2c64ff9cc06bf540507841652b1
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2026-01-19 12:32:16 +0100

    Implement slider hint animation
    Add animation hint for slider movement

--- script.js
+++ script.js
@@ -28,3 +28,24 @@ function update(e) {
   afterImg.style.clipPath = `inset(0 ${100 - percent}% 0 0)`;
   slider.style.left = `${percent}%`;
 }
+// Vis swipe-hint én gang
+setTimeout(() => {
+  animateHint();
+}, 400);
+
+function animateHint() {
+  let pos = 50;
+  let dir = 1;
+  let steps = 0;
+
+  const interval = setInterval(() => {
+    pos += dir * 1.2;
+    if (pos > 58 || pos < 42) dir *= -1;
+
+    afterImg.style.clipPath = `inset(0 ${100 - pos}% 0 0)`;
+    slider.style.left = `${pos}%`;
+
+    steps++;
+    if (steps > 40) clearInterval(interval);
+  }, 16);
+}
